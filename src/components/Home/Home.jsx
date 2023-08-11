cbc6d2aac3c18ea8389ed2a8d6d83a7bffe6face
kamran rahmani <rahmanikamran95@gmail.com>
2023-08-10 22:36:47 -0400

    changed profile photo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -37,7 +37,7 @@ function Home() {
     <Menu />
     <div id='home' className='absolute text-white flex flex-col lg:flex-row justify-center items-center space-y-6 lg:space-y-0 lg:space-x-52 px-4 mt-32 lg:my-0 h-screen w-[90%] z-10'>
         <div className='h-4/5 w-4/5 lg:w-2/5'>
-            <img className='h-full w-full rounded-2xl' src="../../assets/images/slack.jpg" alt="" />
+            <img className='h-full w-full rounded-2xl' src="../../assets/images/kamran.jpg" alt="" />
         </div>
         <Intro /> 
     </div>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -14,6 +14,7 @@ function Portfolio() {
       <Menu />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-32 w-4/5 mx-auto">
         <PortfolioItem languages={'React, Node, Tailwind'} src={'../assets/images/Perla.png'} title={'Perla'} address={'goperla.com'}/>
+        <PortfolioItem languages={'React, Node, Tailwind'} src={'../assets/images/chatapp.png'} title={'chat-app'} address={'github.com/kamranrahmani'}/>
 
         <PortfolioItem languages={'HTML, Tailwind, JS, Node.js, Express.js'} src={'../assets/images/AE.png'} title={'Audio Ecommerce'} address={'audioecommerce.herokuapp.com'}/>
 
